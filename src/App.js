3768973edc640f3698603321e8ec28c0ce29c512
Mauricio Silvera <mauriciosilvera@MBP-de-Mauricio.fibertel.com.ar>
2022-09-21 13:45:48 -0300

    Poll card component first commit with styled components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import PollManagement from './views/PollManagement/PollManagement';
 import TestView from './views/TestView';
+import PollResults from './views/PollManagement/components/PollResults/PollResults';
 
 function App() {
   const location = useLocation();
@@ -29,10 +30,10 @@ function App() {
           isOpen={isSideBarOpen}
           setIsOpen={setIsSideBarOpen}
         />
-        <div className="view">
-          <Routes>
-            <Route path="/" element={<PollManagement />} />
+        <Routes>
+          <Route path="/" element={<TestView />} />
 
+<<<<<<< HEAD
             {/* ADMIN */}
             <Route path="admin/login" element={<TestView />} />
             <Route path="admin/password-recovery" element={<TestView />} />
@@ -40,6 +41,15 @@ function App() {
             <Route path="admin/active-polls" element={<TestView />} />
           </Routes>
         </div>
+=======
+          {/* ADMIN */}
+          <Route path="admin/login" element={<TestView />} />
+          <Route path="admin/password-recovery" element={<TestView />} />
+
+          <Route path="admin/poll-management" element={<TestView />} />
+          <Route path="admin/active-polls" element={<PollResults />} />
+        </Routes>
+>>>>>>> 496eac9 (Poll card component first commit with styled components)
       </div>
     </div>
   );
