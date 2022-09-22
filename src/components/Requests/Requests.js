819454f6fc4ee77da26eb5d4b6485370eef54c99
Mauricio Silvera <mauriciosilvera@MBP-de-Mauricio.fibertel.com.ar>
2022-09-22 17:04:50 -0300

    First BE hookup commit

---
+++ src/components/Requests/Requests.js
@@ -0,0 +1,14 @@
+// import axios from 'axios';
+
+// export const doLogin = (credentials) =>
+//   axios
+//     .post('/user', {
+//       firstName: 'Fred',
+//       lastName: 'Flintstone'
+//     })
+//     .then((response) => {
+//       console.log(response);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
