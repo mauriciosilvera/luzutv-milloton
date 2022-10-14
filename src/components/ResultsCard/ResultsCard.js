0fd831266c99615794a3b43bd9dd0691dc49f2f6
Mauricio Silvera <mauriciosilvera@MBP-de-Mauricio.fibertel.com.ar>
2022-10-14 19:18:02 -0300

    Percentages in group results done

--- src/components/ResultsCard/ResultsCard.js
+++ src/components/ResultsCard/ResultsCard.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './ResultsCard.css';
 
 function ResultsCard(props) {
-  const { name, votes } = props;
+  const { name, votes, percentage } = props;
 
   return (
     <div className="resultsCardWrapper">
       <span className="questionName">{name}</span>
       <div className="percentageBar" />
-      <span className="questionVotes">{`Votos: ${votes}`}</span>
+      <div className="votesResults">
+        <span className="questionVotes">{`Votos: ${votes}`}</span>
+        {percentage && (
+          <span className="questionVotes">{`${percentage}%`}</span>
+        )}
+      </div>
     </div>
   );
 }
