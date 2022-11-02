fca0b87b8f7b066d856c98e45f7d8116acdce458
Alan Nevot <alanccbp@gmail.com>
2022-11-02 02:13:27 -0300

    feat: GraphResultsNewView
    Add index.js files in every Components folders to allow importing the components as modules from the View's components. Also, to avoid using super-large routes when importing modules.

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as PollCard } from './PollCard';
+export { default as PollOption } from './PollOption';
+export { default as ResultsCard } from './ResultsCard';
+export { default as Sidebar } from './Sidebar';
